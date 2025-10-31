52763747364bd87b49d48225efc7cb9a1fbfbd4f
Angel Carregal <angel.hela2@gmail.com>
2025-10-31 13:17:26 -0300

    mas retoques de css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { Nav } from "../Nav/Nav"
+import { Link } from "react-router-dom"
 import "./Header.css"
 
 export const Header = () => {
   return (
     <header>
-      <h2>Ang<span>Dev.</span></h2>
+      <Link to={"/"}><h2>Ang<span>Dev.</span></h2></Link>
       <Nav />
     </header>
   )
