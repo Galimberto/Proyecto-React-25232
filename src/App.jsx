175fae0526e68fffb6794dd709a086c77b7d95b7
Angel Carregal <angel.hela2@gmail.com>
2025-11-05 12:25:16 -0300

    trabajos para el formulario de carga

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { Footer } from "./components/Footer/Footer"
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer"
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer"
 import { CartProvider } from './context/CartContext/CartProvider'
-import './App.css'
 import { Cart } from './components/Cart/Cart'
+import { ProductFormContainer } from './components/adminComponents/ProductFormContainer/ProductFormContainer'
+import './App.css'
 
 function App() {
   
@@ -20,7 +21,8 @@ function App() {
               <Route path='/' element={<ItemListContainer titulo={"Bienvenidos a el catalogo de productos"} />}/>   
               <Route path='/category/:category' element={<ItemListContainer titulo={"Productos por categoria: "} />}/>         
               <Route path='/detail/:id' element={<ItemDetailContainer />} />
-              <Route path='/carrito' element={<Cart/>} />                        
+              <Route path='/carrito' element={<Cart/>} />
+              <Route path='/admin' element={<ProductFormContainer/>} />                          
             </Routes>
           </main>
           <Footer />
